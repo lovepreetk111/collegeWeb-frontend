50332f41da02b4db40c5efa46d1b5c07f450b5ff
Pankaj2130 <pankajvishwakarma23002@gamol.com>
2022-11-04 07:30:54 -0700

    ok

---
+++ src/app/pages/course-sub1/course-sub1.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-course-sub1',
+  templateUrl: './course-sub1.component.html',
+  styleUrls: ['./course-sub1.component.scss']
+})
+export class CourseSub1Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
