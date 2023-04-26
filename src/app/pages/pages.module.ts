29048ff0f520ba8fec4e72845dc0996fd0b0e178
Shubham Todankar <shubhamtodankar55@gmail.com>
2023-04-26 09:07:30 +0530

    Error Page

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { PagesRoutingModule } from './pages-routing.module';
+import { ErrorPageComponent } from './error-page/error-page.component';
 
 console.log("pages Loaded")
 @NgModule({
-  declarations: [],
+  declarations: [
+    ErrorPageComponent
+  ],
   imports: [
     CommonModule,
     PagesRoutingModule
